bcf116064a43d357cfec97876aedf7bfcaaa3d08
Efridthjof <efridthjof@gmail.com>
2018-05-23 11:48:23 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,235 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {StompService} from '@stomp/ng2-stompjs';
+import {Subscription} from 'rxjs/Subscription';
+import {Observable} from 'rxjs/Observable';
+import {Message} from '@stomp/stompjs';
+
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+//     private serverUrl = 'http://192.168.3.129:8080/socket';
+    public title = 'Websocket Test';
+//     private stompClient;
+//     private stompClientLiveData;
+//
+//     constructor() {
+//         this.initializeWebSocketConnection();
+//     }
+//
+//     ngOnInit() {
+//         this.initializeWebSocketConnection();
+//     }
+//
+//     initializeWebSocketConnection() {
+//         const ws = new SockJS(this.serverUrl);
+//
+//         this.stompClient = Stomp.over(ws);
+//         const that = this;
+//         this.stompClient.connect({}, function (frame) {
+//             that.stompClient.subscribe('/data', (message) => {
+//                 if (message.body) {
+//                     $('.chat').append('<div class=\'message\'>' + message.body + '</div>');
+//                 }
+//             });
+//         });
+//     }
+//
+//     sendMessage(message) {
+//         this.stompClient.send('/batchServer/responseData', {}, message);
+//         $('#input').val('');
+//     }
+// }
+
+    // Stream of messages
+    private subscription: Subscription;
+    public messages: Observable<Message>;
+
+    dataIsReady: Boolean = false;
+
+
+    data3Arr: any = [];
+    data2Arr: any = [];
+    data5Arr: any = [];
+    data1Arr: any = [];
+    data4Arr: any = [];
+    chartData: any = [];
+    xAxisData: any = [];
+
+
+    public subscribed: boolean;
+
+    public history: Array<any> = [];
+
+
+    public lineChartOptions: any = {
+        responsive: true,
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    displayFormats: {
+                        second: 'HH:mm:ss',
+                        minute: 'HH:mm',
+                        hour: 'MMM D, H[h]'
+                    }
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Time'
+                },
+            }],
+            yAxes: [{
+                id: 'y-axis-temp',
+                type: 'linear',
+                display: true,
+                position: 'left',
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Temperature'
+                },
+                ticks: {min: 25, max: 90}
+            }, {
+                id: 'y-axis-1',
+                type: 'linear',
+                display: true,
+                position: 'right',
+                scaleLabel: {
+                    display: true,
+                    labelString: 'W/W %'
+                },
+                ticks: {min: 0, max: 25}
+            }, {
+                id: 'y-axis-2',
+                type: 'linear',
+                display: true,
+                position: 'right',
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Carbohydrate Polymerization'
+                },
+                ticks: {min: 0, max: 5}
+            }]
+        }
+    };
+    public lineChartLegend: Boolean = true;
+    public lineChartType: String = 'line';
+
+    constructor(private _stompService: StompService) {
+
+    }
+
+    ngOnInit() {
+        this.subscribed = false;
+
+        this.subscribe();
+    }
+
+    public subscribe() {
+        if (this.subscribed) {
+            return;
+        }
+
+        // Stream of messages
+        this.messages = this._stompService.subscribe('/batchServer/data');
+
+        // Subscribe a function to be run on_next messages
+        this.subscription = this.messages.subscribe(this.onNext, error2 => console.log(error2));
+
+        this.subscribed = true;
+
+    }
+
+    public unsubscribe() {
+        if (!this.subscribed) {
+            return;
+        }
+
+        // This will internally unsubsribe from Stomp Broker
+        this.subscription.unsubscribe();
+        this.subscription = null;
+        this.messages = null;
+
+        this.subscribed = false;
+    }
+
+    ngOnDestroy() {
+        this.unsubscribe();
+    }
+
+    public onNext = (message: Message) => {
+        console.log(message);
+        // Store message in a history Array
+        this.history.push(message.body);
+
+        console.log(JSON.parse(message.body));
+        // this.lineChartData = ;
+        this.loopData(JSON.parse(message.body));
+    };
+
+    // events
+    public chartClicked(e: any): void {
+        console.log(e);
+    }
+
+    public chartHovered(e: any): void {
+        console.log(e);
+    }
+
+    public makeData() {
+        this._stompService.publish('/batchServer/responseData', 'hello');
+    }
+
+    private loopData(loopThis) {
+        for (const data of loopThis) {
+            this.xAxisData.push(data.timestamp);
+            this.data3Arr.push(data.data3);
+            this.data2Arr.push(data.data2 * data.data1);
+            if (data.data1 > 18) {
+                this.data5Arr.push(data.data5 * data.data1);
+            } else {
+                this.data5Arr.push(-1);
+            }
+            this.data1Arr.push(data.data1);
+            this.data4Arr.push(data.data4);
+            this.chartData = [
+                {
+                    data: this.data1Arr,
+                    label: 'Data 1',
+                    yAxisID: 'y-axis-1'
+                },
+                {
+                    data: this.data2Arr,
+                    label: 'Data 2',
+                    yAxisID: 'y-axis-1'
+                },
+                {
+                    data: this.data3Arr,
+                    label: 'Data 3',
+                    yAxisID: 'y-axis-2'
+                },
+                {
+                    data: this.data4Arr,
+                    label: 'Data 4',
+                    yAxisID: 'y-axis-temp'
+                },
+                {
+                    data: this.data5Arr,
+                    label: 'Data 5',
+                    yAxisID: 'y-axis-1'
+                }
+            ];
+        }
+        this.dataIsReady = true;
+    }
+
+    public testBtn() {
+        console.log(this.chartData);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {StompConfig, StompService} from '@stomp/ng2-stompjs';
+import {AppComponent} from './app.component';
+// import {Ng2StompComponent} from './ng2-stomp/ng2-stomp.component'
+import {ChartsModule} from 'ng2-charts';
+import * as SockJS from 'sockjs-client';
 
-import { AppComponent } from './app.component';
+export function receiverProvider() {
+    return new SockJS('http://192.168.3.129:8080/socket');
+}
+
+const stompConfig: StompConfig = {
+    // Which server?
+    url: receiverProvider,
+
+    // Headers
+    // Typical keys: login, passcode, host
+    headers: {
+    //     login: 'guest',
+    //     passcode: 'guest'
+    },
+
+    // How often to heartbeat?
+    // Interval in milliseconds, set to 0 to disable
+    heartbeat_in: 0, // Typical value 0 - disabled
+    heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
+    // Wait in milliseconds before attempting auto reconnect
+    // Set to 0 to disable
+    // Typical value 5000 (5 seconds)
+    reconnect_delay: 10000,
+
+    // Will log diagnostics on console
+    debug: true
+};
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        // Ng2StompComponent
+    ],
+    imports: [
+        BrowserModule,
+        ChartsModule
+    ],
+    providers: [
+        StompService,
+        {
+            provide: StompConfig,
+            useValue: stompConfig
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
